1fdedcc785b9e56ce8900f95916859409fa75b98
Gianbogni <gbogni@udesa.edu.ar>
2024-06-07 11:24:51 -0300

    creacion del middleware para la creacion de prdocutos

---
+++ express-boilerplate/middlewares/createProduct-validator.js
@@ -0,0 +1,21 @@
+const { body } = require("express-validator")
+const db= require("../database/models")
+const bcryptjs = require('bcryptjs');
+
+const createProductValidation = [
+    body("imagen")
+        .notEmpty()
+        .withMessage("Debes cargar una imagen")
+        .bail(),
+    body("nombre")
+        .notEmpty()
+        .withMessage("Debes completar este campo con un nombre")
+        .bail()
+        .isLength({ max: 20 }),
+    body("descrpicion")
+        .notEmpty()
+        .withMessage("Debes poner una descripcion")
+        .bail()
+]
+
+module.exports = createProductValidation
