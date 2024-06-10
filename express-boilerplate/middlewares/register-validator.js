98f406d20c835ac072a4a8146043a90602c13fc4
Lucas-biblos <lbiblos@udesa.edu.ar>
2024-06-10 17:51:17 -0300

    realizando cambios en las vistas, middlewares y controllers

--- express-boilerplate/middlewares/register-validator.js
+++ express-boilerplate/middlewares/register-validator.js
@@ -1,56 +1,47 @@
-const {body} = require("express-validator");
-const bcrypt = require('bcryptjs');
-const db = require('../database/models');
-
+const { body } = require("express-validator")
+const db= require("../database/models")
+const bcryptjs = require('bcryptjs');
 
 const registerValidations = [
-   body("email")
-       .notEmpty()
-       .withMessage("Debes completar tu email")
-       .bail()
-       .isEmail()
-       .withMessage("Debes escribir un correo valido")
-       .custom(function(value, {req}){
-           console.log("value: ", value);
-           return db.User.findOne({
-               where: {email: value}
-           })
-           .then(function(userToDataBase){
-               if(userToDataBase){
-                   throw new Error("El usuario ya existe")
-               }
-           })
-       }),
-   body("password")
-       .notEmpty()
-       .withMessage("Debes completar tu contraseña")
-       .bail()
-       .isLength({min: 5}),
-   body("retypePassword")
-       .notEmpty()
-       .withMessage("Debes completar tu contraeña otra vez")
-       .custom(function(value, {req}){
-           console.log("value: ", value);
-           if (value !== req.body.password) {
-               throw new Error("Las contraseñas no coinciden");
-           }
-           return true;
-       })
-
-
-      
-
-
-
+    body("name")
+    .notEmpty()
+    .withMessage("Debes completar tu nombre"),
+    body("email")
+        .notEmpty()
+        .withMessage("Debes completar tu Email")
+        .bail()
+        .isEmail()
+        .withMessage("Debes escribir un formato de correo valido")
+        .custom(function(value, {req}){
+            return db.User.findOne({
+                where: {email:value}
+            })
+            .then(function(userToLogin){
+                if(!userToLogin){
+                    throw new Error("No existe un usuario con ese email")
+                }
+            })
+        }
+    ),
+    body("password")
+        .notEmpty()
+        .withMessage("Debes Introducir un password")
+        .custom(function(value, {req}){
+            return db.User.findOne({
+                where: {email:req.body.email}
+            })
+            .then(function(user){
+                if(user){
+                    const password = user.password;
+                    const passwordOk= bcryptjs.compareSync(value,password);
+                    if(!passwordOk){
+                        throw new Error("Contraseña incorrecta")
+                    }                    
+                }
+            })
+        })
+        
 
 ]
 
-
-
-
-
-
-module.exports = registerValidations ;
-
-
-
+module.exports = registerValidations;
