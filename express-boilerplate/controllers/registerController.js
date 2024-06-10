594a0e05ad6f60b5aede1f9b36cdd5db9195251d
Nicolascresp <ncrespo@udesa.edu.ar>
2024-06-10 15:05:17 -0300

    .

--- express-boilerplate/controllers/registerController.js
+++ express-boilerplate/controllers/registerController.js
@@ -6,39 +6,8 @@ const op = db.Sequelize.Op;
 
 
 let registerController = {
-    index: function (req, res) {
-        //Mostrar el formulario de registro
-        return res.render('register');
-    },
-    store: function (req, res) {
-        const validationErrors = validationResult(req);
-        console.log("validationErrors : ", validationErrors)
-              
-        if(!validationErrors.isEmpty()){
-            return res.render("register",{
-                errors: validationErrors.mapped(),
-                oldData:req.body
-            })
-        } 
-        const user = {
-            name: req.body.name,
-            email: req.body.email,
-            usuario : req.body.usuario,
-            fecha: req.body.fecha,
-            DNI: req.body.dni,
-            foto: req.body.fecha,
-            password: bcrypt.hashSync(req.body.password, 10), 
-        };
-        //creamos el usuario
-        db.User
-            .create(user)
-            .then(function (user) {
-                return res.redirect("/login");
-            })
-            .catch(function (err) {
-                console.log("Error al guardar el usuario", err);
-            });
-    },
-
+    index: function(req, res){
+        res.send("Hola")
+    }
 };
 module.exports = registerController;
