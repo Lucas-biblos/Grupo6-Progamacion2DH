90acfef2f6324a4dc241d512accc8cce07dd9214
Gianbogni <gbogni@udesa.edu.ar>
2024-06-10 18:29:47 -0300

    agregado de rutas de create and delete en el route de productos

--- express-boilerplate/routes/productos.js
+++ express-boilerplate/routes/productos.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const productocontroller = require("../controllers/productosController");
 const { results } = require("../controllers/mainController");
 const productoController = require("../controllers/productosController");
 const createProductValidation = require('../middlewares/createProduct-validator')
 
-router.get('/', productocontroller.index)
+router.get('/', productoController.index)
 
-router.get('/productoagregado', productocontroller.agregarproductos)
+router.get('/productoagregado', productoController.agregarproductos)
 router.post('/', createProductValidation, productoController.agregarproductos)
-
-
-
+router.get('/create', productoController.create);
 router.post('/delete/:id', productoController.delete);
-router.get("/editMovie/:id", productoController.edit)
-router.post("/update/:id", productoController.updateProd)
+router.get("/edit/:id", productoController.edit);
+router.post("/update/:id", productoController.updateProd);
+router.post('/delete/:id', productoController.delete)
 
 module.exports = router
